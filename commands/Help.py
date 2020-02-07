6b5f17fae713a77936734117cf6ceffd8f194375
christopherwalkerml <christopherwalkerml@gmail.com>
2020-02-07 13:10:58 -0500

    Fixed token integrity

--- commands/Help.py
+++
@@ -1,7 +0,0 @@
-import discord
-
-async def helpCommand(message):
-    embed = discord.Embed(title="MonPyBot", description="Darkolythe's Test Bot. List of commands are:", color=0x308ef7)
-    embed.add_field(name="Gets an amount of pictures from prnt.sc", value="!getpic <prnt scr url> <amount>", inline=False)
-    embed.add_field(name="Shows this text block", value="!help", inline=False)
-    await message.channel.send(embed=embed)
